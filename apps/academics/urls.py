c9f0ec8f6533f1c1ece35a744648605704208014
rja3101 <jorgerivas9725@gmail.com>
2025-11-06 08:30:15 -0500

    Modulo docente

--- apps/academics/urls.py
+++ apps/academics/urls.py
@@ -3,9 +3,11 @@ from django.urls import path
 from .views_enrollment_cart import offerings, cart_view, cart_add, cart_remove, cart_confirm
 from .views_import import import_students, import_enrollments
 from .views_reports import occupancy_report, occupancy_csv
-from .views_grades import import_grades, grades_csv   # <- grades_csv lo agregamos abajo
-from .views_stats import group_stats_view              # <- nombre EXACTO al tuyo
+from .views_grades import import_grades, grades_csv   
+from .views_stats import group_stats_view             
 from . import views_student
+from . import views_teacher   
+
 
 app_name = "academics"
 
@@ -15,7 +17,13 @@ urlpatterns = [
     path("cart/add/<int:group_id>/", cart_add, name="academics_cart_add"),
     path("cart/remove/<int:group_id>/", cart_remove, name="academics_cart_remove"),
     path("cart/confirm/", cart_confirm, name="academics_cart_confirm"),
-
+    
+    path("docente/", views_teacher.teacher_dashboard, name="teacher_dashboard"),
+    path("docente/asistencia/", views_teacher.teacher_attendance, name="teacher_attendance"),
+    path("docente/notas/", views_teacher.teacher_grades, name="teacher_grades"),
+    path("docente/horario/", views_teacher.teacher_schedule, name="teacher_schedule"),
+    path("docente/ambientes/", views_teacher.teacher_rooms, name="teacher_rooms"),
+    
     path("secretary/import/students/", import_students, name="import_students"),
     path("secretary/import/enrollments/", import_enrollments, name="import_enrollments"),
     path("secretary/reports/occupancy/", occupancy_report, name="occupancy_report"),
